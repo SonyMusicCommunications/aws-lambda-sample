14f1976447673eb66d5965ae6b01d52f6cac5ae4
Tomoyuki Shirakawa <tomoyuki.shirakawa@sonymusic.co.jp>
2018-06-25 20:00:54 +0900

    es2015でビルドできるところまで

--- hello-lambda/src/main.handler.ts
+++ hello-lambda/src/main.handler.ts
@@ -1,8 +1,8 @@
 console.log('Loading function');
 
-// exports.handler = async (event, context) => {
-//   console.log('value1 =', event.key1);
-//   console.log('value2 =', event.key2);
-//   console.log('value3 =', event.key3);
-//   return event.key1;
-// }
+exports.handler = async (event, context) => {
+  console.log('value1 =', event.key1);
+  console.log('value2 =', event.key2);
+  console.log('value3 =', event.key3);
+  return event.key1;
+}
